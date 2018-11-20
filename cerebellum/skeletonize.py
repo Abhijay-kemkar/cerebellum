ec13f6ee2038029767e3c7d2ce2b180edc252757
Srujan Meesala <srujanm@gmail.com>
2018-11-19 19:21:26 -0500

    skeletonize.py: added code to update log file, ignore if skeletons already exist

--- cerebellum/skeletonize.py
+++ cerebellum/skeletonize.py
@@ -3,33 +3,41 @@ from ibex.transforms.seg2seg import DownsampleMapping
 from ibex.skeletonization.generate_skeletons import TopologicalThinning, FindEndpointVectors, FindEdges
 
 def check_ids(seg):
-	"""
-	checks if all IDs in segmentation are consecutive
-	Args:
-		seg (ndarray): segmentation data
-	"""
-	seg_ids = np.unique(seg)
-	max_id = np.max(seg_ids)
-	n_ids = len(seg_ids)
-	try:
-	    assert max_id == n_ids-1
-	except:
-	    missing_ids = np.sort(np.array(list(set(range(max_id+1)).difference(set(seg_ids)))))
-	    print "Error! Labels in segmentation are not consecutive. %d IDs are missing"%(len(missing_ids))
-	    print missing_ids
+    """
+    checks if all IDs in segmentation are consecutive
+    Args:
+        seg (ndarray): segmentation data
+    """
+    seg_ids = np.unique(seg)
+    max_id = np.max(seg_ids)
+    n_ids = len(seg_ids)
+    try:
+        assert max_id == n_ids-1
+    except:
+        missing_ids = np.sort(np.array(list(set(range(max_id+1)).difference(set(seg_ids)))))
+        print "Error! Labels in segmentation are not consecutive. %d IDs are missing"%(len(missing_ids))
+        print missing_ids
 
-def gen_skeletons(seg_name, dsmpl_res=(80,80,80)):
-	"""
-	Function to skeletonize a segmentation
-	Args:
-		seg_name (str): name of prepped segmentation
-		dsmpl_res (int, int, int): resolution to downsample segmentation to before skeletonziation in nm
-	"""
-	seg = read3d_h5('./segs/' + seg_name + '/seg.h5', 'main')
-	seg = seg.astype(np.int64)
-	check_ids(seg)
-	create_folder('./skeletons/')
-	DownsampleMapping(seg_name, seg, output_resolution=dsmpl_res)
-	TopologicalThinning(seg_name, seg, skeleton_resolution=dsmpl_res)
-	FindEndpointVectors(seg_name, skeleton_resolution=dsmpl_res)
-	FindEdges(seg_name, skeleton_resolution=dsmpl_res)
+def gen_skeletons(seg_name, dsmpl_res=(80,80,80), overwrite_prev=False):
+    """
+    Function to skeletonize a segmentation
+    Args:
+        seg_name (str): name of prepped segmentation
+        dsmpl_res (int, int, int): resolution to downsample segmentation to before skeletonziation in nm
+    """
+    seg = read3d_h5('./segs/' + seg_name + '/seg.h5', 'main')
+    seg = seg.astype(np.int64)
+    check_ids(seg)
+    create_folder('./skeletons/')
+    if not os.path.isdir('./skeletons/' + seg_name) or overwrite_prev:
+        print "Starting skeletonization of " + seg_name
+        DownsampleMapping(seg_name, seg, output_resolution=dsmpl_res)
+        TopologicalThinning(seg_name, seg, skeleton_resolution=dsmpl_res)
+        FindEndpointVectors(seg_name, skeleton_resolution=dsmpl_res)
+        FindEdges(seg_name, skeleton_resolution=dsmpl_res)
+        log = open("./logs/" + seg_name +'.log', "a+")
+        log.write("completed skeletonization procedure; skeleton count\n")
+        log.write("%d\n"%(np.max(seg)+1))
+        log.close()
+    else:
+        print "Skeletonization on " + seg_name + " was already completed"
--- seg_skeletonize.py
+++ seg_skeletonize.py
@@ -9,18 +9,16 @@ with open('data_locs.json') as f:
 	data_locs = json.load(f)
 
 # TO CHANGE
-block_indices = range(1)
+block_indices = range(2)
 
 # GT blocks
 for i in block_indices:
 	zz = i*data_locs["block-size"]+data_locs["aff-offset"]
 	gt_name = "gt%04d"%(zz)
-	print "Starting skeletonization of " + gt_name
-	gen_skeletons(gt_name)
+	gen_skeletons(gt_name, overwrite_prev=False)
 
 # pred blocks
 for i in block_indices:
 	zz = i*data_locs["block-size"]+data_locs["aff-offset"]
 	pred_name = "pred-pf-crop2gt-%04d"%(zz)
-	print "Starting skeletonization of " + pred_name
-	gen_skeletons(pred_name)
+	gen_skeletons(pred_name, overwrite_prev=False)
