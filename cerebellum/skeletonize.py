d80b2e84ae447e6369ef08d554ebb94665f8baeb
Srujan Meesala <srujanm@gmail.com>
2018-12-05 12:01:35 -0500

    skeletonize.py: gen_skeletons() now supports skeletonization of segmentations at various stages in pipeline

--- cerebellum/skeletonize.py
+++ cerebellum/skeletonize.py
@@ -1,6 +1,10 @@
-from utils.data_io import *
+import numpy as np
+# ibex imports
 from ibex.transforms.seg2seg import DownsampleMapping
 from ibex.skeletonization.generate_skeletons import TopologicalThinning, FindEndpointVectors, FindEdges
+
+from cerebellum.utils.data_io import *
+from cerebellum.data_prep.seg_prep import *
 
 def check_ids(seg):
     """
@@ -18,26 +22,36 @@ def check_ids(seg):
         print "Error! Labels in segmentation are not consecutive. %d IDs are missing"%(len(missing_ids))
         print missing_ids
 
-def gen_skeletons(seg_name, dsmpl_res=(80,80,80), overwrite_prev=False):
+def gen_skeletons(seg_name, resolution, stage=None, dsmpl_res=(80,80,80), overwrite_prev=False):
     """
     Function to skeletonize a segmentation
     Args:
         seg_name (str): name of prepped segmentation
+        stage (str): stage of segmentation in pipeline for skeletonization, eg. "filtered", "tracked", etc.
         dsmpl_res (int, int, int): resolution to downsample segmentation to before skeletonziation in nm
     """
-    seg = read3d_h5('./segs/' + seg_name + '/seg.h5', 'main')
-    seg = seg.astype(np.int64)
-    check_ids(seg)
+    seg = SegPrep(seg_name, resolution)
+    seg.read_internal(stage=stage)
+    if stage is not None:
+        if overwrite_prev is False:
+            print "I currently do not support saving different skeleton files for each stage. "+\
+                    "You can choose to overwrite previous skeletons if you wish."
+        else:
+            print "Warning: overwriting previously generated skeletons, possibly from a different " +\
+                    "stage of %s"%(seg_name)
+    check_ids(seg.data)
+    seg_for_skel = seg.data.astype(np.int64)
     create_folder('./skeletons/')
-    if not os.path.isdir('./skeletons/' + seg_name) or overwrite_prev:
+    create_folder('./skeletons/' + seg_name)
+    if overwrite_prev:
         print "Starting skeletonization of " + seg_name
-        DownsampleMapping(seg_name, seg, output_resolution=dsmpl_res)
-        TopologicalThinning(seg_name, seg, skeleton_resolution=dsmpl_res)
+        DownsampleMapping(seg_name, seg_for_skel, output_resolution=dsmpl_res)
+        TopologicalThinning(seg_name, seg_for_skel, skeleton_resolution=dsmpl_res)
         FindEndpointVectors(seg_name, skeleton_resolution=dsmpl_res)
         FindEdges(seg_name, skeleton_resolution=dsmpl_res)
         log = open("./logs/" + seg_name +'.log', "a+")
         log.write("completed skeletonization procedure; skeleton count\n")
-        log.write("%d\n"%(np.max(seg)+1))
+        log.write("%d\n"%(np.max(seg.data)+1))
         log.close()
     else:
         print "Skeletonization on " + seg_name + " was already completed"
