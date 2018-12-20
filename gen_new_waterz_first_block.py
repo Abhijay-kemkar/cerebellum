0b7748e8e1728f7544a6a426dfcdfab1d054041c
Srujan Meesala <srujanm@gmail.com>
2018-12-20 11:33:15 -0500

    added scripts to run new segmentations thru SegPrep and error analysis modules

---
+++ gen_new_waterz_first_block.py
@@ -0,0 +1,33 @@
+from cerebellum.data_prep.seg_prep import *
+
+
+wz_threshes = [0.5]
+
+###
+# CMD ARGS
+parser = argparse.ArgumentParser(description='run SegPrep on generated segmentation')
+parser.add_argument('-i', '--block_index', type=int, default=0, help='block index')
+args = parser.parse_args()
+i = args.block_index
+# SET PARAMS
+resolution = (30, 48, 48)
+dsmpl = (1,6,6) # from 8 nm
+seg8nm_folder = "/home/srujanm/cerebellum/data/retrained_vol1/segs/"
+block_size = 60
+seg_type = 'train49K'
+###
+
+for wz_id, wz_thresh in enumerate(wz_threshes):
+	# load and prepare pred
+	print "Preparing 0th block for waterz: %.2f"%(wz_thresh)
+	zz = i*block_size
+	seg8nm_file = seg8nm_folder + "seg_" + seg_type + "_%04d_aff85_his256_%.2f.hdf"%(zz, wz_thresh)
+	seg_block_name = "retrained-" + seg_type + "-%04d-wz%.2f"%(zz, wz_thresh)
+	seg_block = SegPrep(seg_block_name, resolution)
+	seg_block.read(seg8nm_file, "main", dsmpl=dsmpl)
+	print seg_block.shape
+	seg_block.write()
+	# STOP HERE FOR UNFILTERED SEGMENTATION
+	# Generate bboxes for fiber filtering
+	# seg_block.gen_bboxes() # Note: Longest step in pipeline
+	# Assuming bboxes are generated, proceed to filter
